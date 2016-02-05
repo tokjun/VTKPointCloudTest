41ba86ceb2558aaa5140e4a9c7d622b79d414bf8
Junichi Tokuda <tokuda@bwh.harvard.edu>
2016-02-04 21:49:44 -0500

    ENH: Initial version.

---
+++ vtkPointCloud.remote.cmake
@@ -0,0 +1,9 @@
+#
+# Dicom Classes
+#
+
+vtk_fetch_module(vtkPointCloud
+  "Dicom classes and utilities"
+  GIT_REPOSITORY git@gitlab.kitware.com:vtk/point-cloud.git
+  GIT_TAG eac1fbe352f33f212b9b02e1c4ebd3812ec05125
+  )
